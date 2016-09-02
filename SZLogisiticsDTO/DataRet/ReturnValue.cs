75c1bf03a9664b6bfa0460fd37b66e58b253a61a
eeee3e3e <yy_double_y@126.com>
2016-09-02 16:58:16 +0800

    0902

---
+++ SZLogisiticsDTO/DataRet/ReturnValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZLogisiticsDTO.DataRet
+{
+    public class ReturnValue
+    {
+        public string RetValue { get; set; }
+        public string RetStatus { get; set; }
+    }
+}
