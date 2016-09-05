c5fd61e0fbc7e2b18382616a67c555b9620cdb0f
eeee3e3e <yy_double_y@126.com>
2016-09-05 16:35:09 +0800

    0905

---
+++ SZLogistics/Controllers/PMainFrmController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SZLogistics.Controllers
+{
+    public class PMainFrmController : Controller
+    {
+        // GET: PMainFrm
+        public ActionResult PMainFrm()
+        {
+            return View();
+        }
+
+
+    }
+}
