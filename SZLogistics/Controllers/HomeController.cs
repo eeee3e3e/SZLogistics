4c7af651334c752864d18991cbb792abe1f5f103
eeee3e3e <yy_double_y@126.com>
2016-09-05 15:18:07 +0800

    0905-2

--- SZLogistics/Controllers/HomeController.cs
+++ SZLogistics/Controllers/HomeController.cs
@@ -14,16 +14,28 @@ namespace SZLogistics.Controllers
             return View();
         }
 
-        public ActionResult LoginSys(string strUserName, string strPassword)
+        public ActionResult LoginSys(string strUserName, string strPassword,string strUserType)
         {
 
             ReturnValue<string> rv = new ReturnValue<string>();
-            SZLogisiticsDTO.SZLogisticsEFContainer SzBuyerEntities = new SZLogisiticsDTO.SZLogisticsEFContainer();
-            var cls = (from UsObj in SzBuyerEntities.T_HUserInfo
-                       where UsObj.F_UserName == strUserName 
-                       select UsObj);
-            rv.RetStatus = cls.Count<SZLogisiticsDTO.T_HUserInfo>() == 0 ? "失败" : "成功";
-            rv.RetValue = cls.Count<SZLogisiticsDTO.T_HUserInfo>() == 0 ? "失败" : "成功";
+            if (strUserType == "1")
+            {
+                SZLogisiticsDTO.SZLogisticsEFContainer SzBuyerEntities = new SZLogisiticsDTO.SZLogisticsEFContainer();
+                var cls = (from UsObj in SzBuyerEntities.T_HUserInfo
+                           where UsObj.F_UserName == strUserName
+                           select UsObj);
+                rv.RetStatus = cls.Count<SZLogisiticsDTO.T_HUserInfo>() == 0 ? "失败" : "成功";
+                rv.RetValue = cls.Count<SZLogisiticsDTO.T_HUserInfo>() == 0 ? "失败" : "成功";
+            }
+            else
+            {
+                SZLogisiticsDTO.SZLogisticsEFContainer SzBuyerEntities = new SZLogisiticsDTO.SZLogisticsEFContainer();
+                var cls = (from UsObj in SzBuyerEntities.T_PUserInfo
+                           where UsObj.F_UserName == strUserName
+                           select UsObj);
+                rv.RetStatus = cls.Count<SZLogisiticsDTO.T_PUserInfo>() == 0 ? "失败" : "成功";
+                rv.RetValue = cls.Count<SZLogisiticsDTO.T_PUserInfo>() == 0 ? "失败" : "成功";
+            }
             return Json(rv,JsonRequestBehavior.AllowGet);
         }
 
